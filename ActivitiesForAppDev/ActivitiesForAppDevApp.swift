96b5b05da8477b57fa85755c66b285c788e52c62
UltiimateDog <alan.perezhz@icloud.com>
2026-01-23 12:34:28 -0600

    Initial Commit

---
+++ ActivitiesForAppDev/ActivitiesForAppDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivitiesForAppDevApp.swift
+//  ActivitiesForAppDev
+//
+//  Created by Ultiimate Dog on 23/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ActivitiesForAppDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivitiesForAppDev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ActivitiesForAppDev
+//
+//  Created by Ultiimate Dog on 23/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
